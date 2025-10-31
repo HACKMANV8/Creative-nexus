ec5ab9425518ad062664b1e55f2e72bb64fb3207
impanam758-cyber <impanam758@gmail.com>
2025-10-31 13:25:35 +0530

    Add files via upload

---
+++ hackman.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+class hospital
+{
+    public:
+    string H_name;
+    string location;
+    int available_beds;
+    float rating;
+    string contact;
+    string doctor_name;
+    int price;
+};
+class Patient: public hospital
+{
+    public:
+    string P_name;
+    int P_id;
+} ;
+void printhospitaldata(vector<hospital>&hospitals)
+{
+    cout << "PRINT hospitals DATA:"
+         << endl;
+
+    cout << "HospitalName     "
+         << "Location     "
+         << "Beds_Available     "
+         << "Rating     "
+         << "Hospital_Contact     "
+         << "Doctor_Name     "
+         << "Price_Per_Bed     \n";
+for (int i = 0; i < 4; i++) {
+        cout << hospitals[i].H_name
+             << "                 "
+             << "        "
+             << hospitals[i].location
+             << "           "
+             << hospitals[i].available_beds
+             << "                    "
+             << hospitals[i].rating
+             << "            "
+             << hospitals[i].contact
+             << "             "
+             << hospitals[i].doctor_name
+             << "                  "
+             << "        "
+             << hospitals[i].price
+             << "            "
+             << endl;
+    }
+    cout << endl
+         << endl;
+}
